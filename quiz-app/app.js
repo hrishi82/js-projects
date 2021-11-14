d5e2322d4c870d99e2c1cbb01c9af85b8d6b666a
Barman Hrishikesh <hrishikesh.barman@volvo.com>
2021-11-15 02:26:23 +0530

    updated html and js

---
+++ quiz-app/app.js
@@ -0,0 +1,33 @@
+let quizData = [{
+        question: 'Whats my hobby?',
+        a: 'Reading Books',
+        b: 'Painting',
+        c: 'Playing Basketball',
+        d: 'Playing guitar',
+        correct: 'd',
+    },
+    {
+        question: 'Where am I from?',
+        a: 'Assam',
+        b: 'Meghalaya',
+        c: 'Karnataka',
+        d: 'Delhi',
+        correct: 'a',
+    },
+    {
+        question: 'Whats 1 + 1?',
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4',
+        correct: 'b',
+    },
+    {
+        question: 'Whats my fav word?',
+        a: 'lol',
+        b: 'Lite',
+        c: 'okay',
+        d: 'cool',
+        correct: 'b',
+    }
+]
