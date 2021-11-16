e39761e29b557849e12e918e07cf605f49d05fc3
Barman Hrishikesh <hrishikesh.barman@volvo.com>
2021-11-17 00:07:24 +0530

    code updated

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -1,9 +1,9 @@
 let quizData = [{
-        question: 'Whats my hobby?',
-        a: 'Reading Books',
-        b: 'Painting',
-        c: 'Playing Basketball',
-        d: 'Playing guitar',
+        question: 'Which is the best Air Jordan silhoette till date?',
+        a: 'Air Jordan 11',
+        b: 'Air Jordan 3',
+        c: 'Air Jordan 4',
+        d: 'Air Jordan 1',
         correct: 'd',
     },
     {
@@ -31,3 +31,60 @@ let quizData = [{
         correct: 'b',
     }
 ]
+
+const questionEl = document.getElementById('question');
+const answerRadioBtn = document.querySelectorAll('.answer');
+const submitBTN = document.getElementById('submit-btn')
+const quizContainer = document.querySelector('.quiz-container')
+
+
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+
+
+questionIndex = 0
+score = 0
+
+function loadQuestion() {
+    questionEl.innerHTML = quizData[questionIndex].question
+    a_text.innerHTML = quizData[questionIndex].a
+    b_text.innerHTML = quizData[questionIndex].b
+    c_text.innerHTML = quizData[questionIndex].c
+    d_text.innerHTML = quizData[questionIndex].d
+}
+
+loadQuestion()
+
+
+function uncheckAllRadioButtons() {
+    answerRadioBtn.forEach(btn => {
+        btn.checked = false;
+    })
+}
+
+function evaluateAnswer() {
+    answerRadioBtn.forEach(btn => {
+        if (btn.checked === true) {
+            if (btn.id == quizData[questionIndex].correct) {
+                score++
+            }
+        }
+
+    })
+}
+
+submitBTN.addEventListener('click', () => {
+    evaluateAnswer()
+    questionIndex++
+    if (questionIndex < quizData.length) {
+        uncheckAllRadioButtons()
+        loadQuestion()
+    } else {
+        quizContainer.innerHTML = `You scored ${score}/${quizData.length}!`
+        quizContainer.style.padding = '5rem';
+        quizContainer.style.fontWeight = 'bold';
+    }
+
+})
